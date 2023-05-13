b44ec901d559975aba9ac146e0235b660caa9254
SiDawg <s.h.anderson101@gmail.com>
2023-05-13 15:09:06 +1000

    testing publish batch file

---
+++ publish.bat
@@ -0,0 +1,14 @@
+
+REM Check if an argument is provided
+IF "%~1"=="" (
+    echo Error: Please provide a commit message as an argument.
+    exit /b 1
+)
+
+REM Execute Git commands
+git add .
+git commit -m "%~1"
+git push origin main
+
+REM Exit the batch file
+exit /b 0
